e3f28213cde80650345d38279a941fa0667f16ab
beautea37 <beautea37@gmail.com>
2024-03-06 22:51:04 +0900

    시큐리티 해제 성공!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/com/me/kije/promptproject/config/WebSecurityConfig.java
+++ src/main/java/com/me/kije/promptproject/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.me.kije.promptproject.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -9,14 +11,14 @@ public class WebSecurityConfig {
 
 
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests(authorizeRequests ->
-//                        authorizeRequests.anyRequest().permitAll()
-//                )
-//                .csrf().disable(); // CSRF 보호 기능을 비활성화합니다. 테스트 목적으로만 사용하세요.
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll()
+                )
+                .csrf().disable(); // CSRF 보호 기능을 비활성화합니다. 테스트 목적으로만 사용하세요.
+
+        return http.build();
+    }
 }
